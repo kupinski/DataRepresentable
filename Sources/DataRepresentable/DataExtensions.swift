aa551a29bae8a0bfcb5a60a82b695650df053693
Matthew Kupinski <mkupinski@optics.arizona.edu>
2021-09-01 13:48:28 -0700

    process function now public

--- Sources/DataRepresentable/DataExtensions.swift
+++ Sources/DataRepresentable/DataExtensions.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 extension Data {
-    mutating func process<C: DataRepresentable>() -> C {
+    public mutating func process<C: DataRepresentable>() -> C {
         let val = self.withUnsafeBytes {$0.load(as: C.self)}
         self = self.advanced(by: MemoryLayout<C>.stride)
         return(val)
     }
     
-    mutating func process<C>(count: Int) -> [C] {
+    public mutating func process<C>(count: Int) -> [C] {
         let arr = Array<C>(unsafeUninitializedCapacity: count,
                      initializingWith: {(buf, initCount) in
             _ = self.copyBytes(to: buf)
